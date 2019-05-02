560f9050cf5869f6afa110e108891c41826fe541
Mohammad Javad Mohseni Far <mohammadjavadmohsenifar@Mohammads-MacBook-Pro.local>
2019-05-02 08:21:06 -0700

    html-css

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var sec = document.querySelector("section");
+for (var i = 0; i < 49; i++) {
+  var cell = document.createElement("IMG");
+  cell.setAttribute("src", "images/facingDown.png");
+sec.appendChild(cell);
+}
