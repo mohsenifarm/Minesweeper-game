780f260004ec17d4e64b6360e720940f6cee854a
Mohammad Javad Mohseni Far <mohammadjavadmohsenifar@Mohammads-MacBook-Pro.local>
2019-05-08 10:27:07 -0700

    fix-all-bugs

--- js/main.js
+++ js/main.js
@@ -17,33 +17,44 @@ const pics = {
 var mineArr, img, btn, row, column, minesNum, flagArr;
 
 //---------------dom--------------------
+var inp = document.querySelector("input").value;
 var sec = document.querySelector("section");
-var resetBtn = document.querySelector("button");
+var resetBtn = document.querySelector("#refresh");
 var h1 = document.querySelector("h1");
+var start = document.querySelector("#start");
 
 //----------event & listener------------
 
 sec.addEventListener("click", play);
+sec.addEventListener("contextmenu", getFlag);
+start.addEventListener("click", startGame);
+resetBtn.addEventListener("click", resrtGame);
 //---------------function----------------
 init();
 function init() {
   flagArr = [];
   mineArr = [];
+  row = 10;
+  column = 10;
+
+  minesNum = 1;
   render();
   img = document.querySelectorAll("img");
-  row = 7;
-  column = 7;
-  minesNum = 3;
 }
 function render() {
-  for (var i = 0; i < 49; i++) {
+  for (var i = 0; i < row * column; i++) {
     var cell = document.createElement("IMG");
     cell.id = i.toString();
     cell.setAttribute("src", "images/facingDown.png");
     sec.appendChild(cell);
   }
 }
-resetBtn.addEventListener("click", resrtGame);
+// function startGame(evt) {
+//   minesNum = parseInt(inp);
+//   // console.log(minesNum);
+// }
+// startGame();
+
 function resrtGame(event) {
   event.target.id = location.reload();
 }
@@ -54,10 +65,10 @@ function play(evt) {
   if (mineArr.includes(parseInt(eti))) {
     mineArr.forEach(elm => {
       img[elm].src = `${pics.bomb}`;
-      h1.innerHTML = `sorry you loose the game &#128546`;
-      setTimeout(function() {
-        location.reload();
-      }, 2000);
+      h1.innerHTML = `sorry you lose the game &#128546`;
+      // setTimeout(function() {
+      //   location.reload();
+      // }, 2000);
     });
   } else {
     if (mineFinder(x, y) === 0) {
@@ -99,7 +110,7 @@ function isMine(x, y) {
 function getRandom() {
   var i = 0;
   while (i < minesNum) {
-    var num = Math.floor(Math.random() * 49);
+    var num = Math.floor(Math.random() * 100);
     if (!mineArr.includes(num)) {
       mineArr.push(num);
       i++;
@@ -121,58 +132,40 @@ function checkImage(x, y) {
   }
 }
 //put flag
-sec.addEventListener("contextmenu", getFlag);
 function getFlag(event) {
   event.preventDefault();
-
-  if (
-    (img[event.target.id] =
-      `${pics.empty}` && img[event.target.id] != `${pics.zero}`)
-  ) {
+  console.log(img[event.target.id].attributes.src.nodeValue);
+  if (img[event.target.id].attributes.src.nodeValue === `${pics.empty}`) {
     img[event.target.id].src = `${pics.flag}`;
     flagArr.push(parseInt(event.target.id));
     // console.log(flagArr);
-  } else if (
-    (img[event.target.id] =
-      `${pics.flag}` && img[event.target.id] != `${pics.zero}`)
-  ) {
+  } else if (img[event.target.id].attributes.src.nodeValue === `${pics.flag}`) {
     img[event.target.id].src = `${pics.empty}`;
   }
   getWinner();
 }
-// {flagArr.forEach(falgelm)=>{
-//   if(minelm === falgelm
 
 function getWinner() {
   var a = mineArr.sort();
   var b = flagArr.sort();
-  // console.log(a);
-  // console.log(b);
-  // console.log(parseInt(a.length));
-  // console.log(b.length);
 
   for (var i = 0; i < a.length; i++) {
     if (a[i] === b[i] && b.length === a.length) {
       h1.innerHTML = "YoU ArE A WiNnEr!!!!!!!!";
     }
   }
-
-  // if (
-  //   mineArr.includes(flagArr.forEach(function(elm){
-  //     return elm;
-  //   })
-
-  // )) alert('win');
 }
 
 function reveal(x, y) {
   if (mineFinder(x, y) === 0) {
     img[y * row + x].src = `images/0.png`;
   }
+
   if (mineFinder(x - 1, y) === 0 && x > 0 && x < row && checkImage(x - 1, y))
     reveal(x - 1, y);
-  else if (mineFinder(x - 1, y) !== 0)
+  else if (mineFinder(x - 1, y) !== 0 && x > 0 && x < row)
     img[y * row + x - 1].src = `images/${mineFinder(x - 1, y)}.png`;
+
   if (
     mineFinder(x + 1, y) === 0 &&
     x >= 0 &&
@@ -180,12 +173,14 @@ function reveal(x, y) {
     checkImage(x + 1, y)
   )
     reveal(x + 1, y);
-  else if (mineFinder(x + 1, y) !== 0 && checkImage(x + 1, y))
+  else if (mineFinder(x + 1, y) !== 0 && x >= 0 && x < row - 1)
     img[y * row + (x + 1)].src = `images/${mineFinder(x + 1, y)}.png`;
+
   if (mineFinder(x, y - 1) === 0 && y > 0 && y < column && checkImage(x, y - 1))
     reveal(x, y - 1);
-  else if (mineFinder(x, y - 1) !== 0)
+  else if (mineFinder(x, y - 1) !== 0 && y > 0 && y < column)
     img[(y - 1) * row + x].src = `images/${mineFinder(x, y - 1)}.png`;
+
   if (
     mineFinder(x, y + 1) === 0 &&
     y >= 0 &&
@@ -193,76 +188,73 @@ function reveal(x, y) {
     checkImage(x, y + 1)
   )
     reveal(x, y + 1);
-  else if (mineFinder(x, y + 1) !== 0)
+  else if (mineFinder(x, y + 1) !== 0 && y >= 0 && y < column - 1)
     img[(y + 1) * row + x].src = `images/${mineFinder(x, y + 1)}.png`;
 
-  // if (
-  //   mineFinder(x - 1, y - 1) === 0 &&
-  //   x > 0 &&
-  //   y > 0 &&
-  //   y < column &&
-  //   x < row &&
-  //   checkImage(x - 1, y - 1)
-  // )
-  //   reveal(x - 1, y - 1);
-  // else if (mineFinder(x - 1, y - 1) !== 0) {
-  //   if (img[y * row + x - (row + 1)]) {
-  //     img[y * row + x - (row + 1)].src = `images/${mineFinder(
-  //       x - 1,
-  //       y - 1
-  //     )}.png`;
-  //   }
-  // }
+  if (
+    mineFinder(x - 1, y - 1) === 0 &&
+    x > 0 &&
+    x < row &&
+    y > 0 &&
+    checkImage(x - 1, y - 1)
+  )
+    reveal(x - 1, y - 1);
+  else if (mineFinder(x - 1, y - 1) !== 0 && y > 0 && x > 0 && x < row)
+    img[y * row + x - (row + 1)].src = `images/${mineFinder(x - 1, y - 1)}.png`;
 
-  // if (
-  //   mineFinder(x + 1, y + 1) === 0 &&
-  //   x >= 0 &&
-  //   x < row - 1 &&
-  //   y >= 0 &&
-  //   y < column -1 &&
-  //   checkImage(x + 1, y + 1)
-  // )
-  //   reveal(x + 1, y + 1);
-  // else if (mineFinder(x + 1, y + 1) !== 0) {
-  //   if (img[(y + 1) * row + (x + 1)]) {
-  //     img[(y + 1) * row + (x + 1)].src = `images/${mineFinder(
-  //       x + 1,
-  //       y + 1
-  //     )}.png`;
-  //   }
-  // }
-  // if (
-  //   mineFinder(x + 1, y - 1) === 0 &&
-  //   x >= 0 &&
-  //   x < row - 1 &&
-  //   y > 0 &&
-  //   y < column &&
-  //   checkImage(x + 1, y - 1)
-  // )
-  //   reveal(x + 1, y - 1);
-  // else if (mineFinder(x + 1, y - 1) !== 0) {
-  //   if (img[(y - 1) * row + (x + 1)]) {
-  //     img[(y - 1) * row + (x + 1)].src = `images/${mineFinder(
-  //       x + 1,
-  //       y - 1
-  //     )}.png`;
-  //   }
-  // }
-  // if (
-  //   mineFinder(x - 1, y + 1) === 0 &&
-  //   x > 0 &&
-  //   x < row - 1 &&
-  //   y >= 0 &&
-  //   y < column &&
-  //   checkImage(x - 1, y + 1)
-  // )
-  //   reveal(x - 1, y + 1);
-  // else if (mineFinder(x - 1, y + 1) !== 0) {
-  //   if (img[(y + 1) * row + (x - 1)]) {
-  //     img[(y + 1) * row + (x - 1)].src = `images/${mineFinder(
-  //       x - 1,
-  //       y + 1
-  //     )}.png`;
-  //   }
-  // }
+  if (
+    mineFinder(x + 1, y + 1) === 0 &&
+    x >= 0 &&
+    x < row - 1 &&
+    y >= 0 &&
+    y < column - 1 &&
+    checkImage(x + 1, y + 1)
+  )
+    reveal(x + 1, y + 1);
+  else if (
+    mineFinder(x + 1, y + 1) !== 0 &&
+    x >= 0 &&
+    x < row - 1 &&
+    y >= 0 &&
+    y < column - 1
+  )
+    img[(y + 1) * row + (x + 1)].src = `images/${mineFinder(x + 1, y + 1)}.png`;
+
+  if (
+    mineFinder(x + 1, y - 1) === 0 &&
+    x >= 0 &&
+    x < row - 1 &&
+    y > 0 &&
+    y < column &&
+    checkImage(x + 1, y - 1)
+  )
+    reveal(x + 1, y - 1);
+  else if (
+    mineFinder(x + 1, y - 1) !== 0 &&
+    x >= 0 &&
+    x < row - 1 &&
+    y > 0 &&
+    y < column
+  ) {
+    img[(y - 1) * row + (x + 1)].src = `images/${mineFinder(x + 1, y - 1)}.png`;
+  }
+
+  if (
+    mineFinder(x - 1, y + 1) === 0 &&
+    x > 0 &&
+    x < row &&
+    y >= 0 &&
+    y < column - 1 &&
+    checkImage(x - 1, y + 1)
+  )
+    reveal(x - 1, y + 1);
+  else if (
+    mineFinder(x - 1, y + 1) !== 0 &&
+    x > 0 &&
+    x < row &&
+    y >= 0 &&
+    y < column - 1
+  ) {
+    img[(y + 1) * row + (x - 1)].src = `images/${mineFinder(x - 1, y + 1)}.png`;
+  }
 }
